a3686cdc546247aa96e6aec95fa14626ffef2aa4
jimmyRobson <blobeyes@gmail.com>
2013-11-15 20:26:16 -0900

    moved all of the js from the index except that tab stuff here. add custom options. fixed all back button bugs.

--- phpHTML_2/buildTabs.js
+++ phpHTML_2/buildTabs.js
@@ -1,23 +1,84 @@
-<script src="http://code.jquery.com/jquery-1.6.min.js"></script>
-<script src="acidTabs.js"></script>
-<script>
+history.navigationMode = 'compatible';
 $(document).ready(function(){
-   $("#tabContainer").acidTabs();
+   $('#tabContainer').acidTabs();
+   
+    //logic for Default or Custom setting
+    
+    
+    $('.Type').each(function(){
+    
+
+
+    if($(this).val()==='default'){
+        $(this).parent().parent().find('.iterativeDiv').hide();
+        $(this).parent().parent().find('.customQuestionDiv').hide();
+        //alert();
+    }
+    else{
+        $(this).parent().parent().find('.iterativeDiv').show();
+        $(this).parent().parent().find('.customQuestionDiv').show();
+        if($(this).parent().parent().find('.iterativeCheckbox').is(':checked')){
+            $(this).parent().parent().find('.numIterativeQuestions').removeAttr('disabled');
+        }
+        else{
+            $(this).parent().parent().find('.numIterativeQuestions').attr('disabled', 'disabled').val('');
+        }
+    }
+
+
+ });
+    
+    
+    
+    $('.Type').click(function (){//if they change it after the page loads
+       
+        if($(this).val()==='default'){
+           
+            $(this).parent().parent().find('.iterativeDiv').hide();
+            $(this).parent().parent().find('.customQuestionDiv').hide();
+        }
+        else{//Custom Choice
+             $(this).parent().parent().find('.iterativeDiv').show();
+            $(this).parent().parent().find('.customQuestionDiv').show();
+           if($(this).parent().parent().find('.iterativeCheckbox').is(':checked')){
+            $(this).parent().parent().find('.numIterativeQuestions').removeAttr('disabled').val('10');
+            }
+            else{
+            $(this).parent().parent().find('.numIterativeQuestions').attr('disabled', 'disabled').val('');
+            }
+        
+        }
+   
+    });
+    
+    $('.iterativeCheckbox').click(function (){
+        if($(this).is(':checked')){
+            $(this).parent().parent().find('.numIterativeQuestions').removeAttr('disabled').val('10');
+            }
+            else{
+            $(this).parent().parent().find('.numIterativeQuestions').attr('disabled', 'disabled').val('');
+            }
+    
+    
+    
+    });
+   
+        //logic for Risk MPL (constant dollar and probability.
 	var $A1 = $('#a1Final'),
 	$A2 = $('#a2Final'),
 	$B1 = $('#b1Final'),
-    $B2 = $('#b2Final'),
-	$sides = $('#sides')
+        $B2 = $('#b2Final'),
+	$sides = $('#sides'),
 	$a1Prob =$('#a1Prob'),
 	$a2Prob =$('#a2Prob'),
 	$b1Prob =$('#b1Prob'),
-	$b2Prob =$('#b2Prob');
-	/*$a1Prob.attr('disabled', 'disabled').val('');
-	$a2Prob.attr('disabled', 'disabled').val('');
-	$b1Prob.attr('disabled', 'disabled').val('');
-	$b2Prob.attr('disabled', 'disabled').val('');*/
+	$b2Prob =$('#b2Prob'),
+	$constDollarDisplay =$('#exampleRisk'),
+	$constProbabiltyDisplay=$('#exampleRisk2');
         
-        if($("input:radio[name=constant]").val()=='dollar'){
+        if($('#dollar').is(':checked')){//BUG has been fixed
+			$constDollarDisplay.show();
+			$constProbabiltyDisplay.hide();
                         $A1.attr('disabled', 'disabled').val('');
 			$A2.attr('disabled', 'disabled').val('');
 			$B1.attr('disabled', 'disabled').val('');
@@ -29,12 +90,16 @@ $(document).ready(function(){
 			$b2Prob.attr('disabled', 'disabled').val('');
         }
         else{
+						$constDollarDisplay.hide();
+						$constProbabiltyDisplay.show();
                         $sides.attr('disabled', 'disabled').val('');
         }
         
-	$("input:radio[name=constant]").click(function (){
+	$('input:radio[name=constant]').click(function (){
     var constVal = $(this).val();  
     	if (constVal=='dollar'){
+			$constDollarDisplay.show();
+			$constProbabiltyDisplay.hide();
 			$A1.attr('disabled', 'disabled').val('');
 			$A2.attr('disabled', 'disabled').val('');
 			$B1.attr('disabled', 'disabled').val('');
@@ -49,6 +114,9 @@ $(document).ready(function(){
 			
 		}
 		else{
+			$constDollarDisplay.hide();
+			$constProbabiltyDisplay.show();
+
 			$A1.removeAttr('disabled');
 			 $A2.removeAttr('disabled');
 			$B1.removeAttr('disabled');
@@ -64,4 +132,4 @@ $(document).ready(function(){
 		}
   });
  });
-</script>
+ 
