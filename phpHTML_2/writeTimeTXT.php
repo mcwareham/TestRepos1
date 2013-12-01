9322895478c1317bfc2ee6763e7552cd88ee3f84
jimmyRobson <blobeyes@gmail.com>
2013-11-30 18:47:42 -0900

    forgot so save

--- phpHTML_2/writeTimeTXT.php
+++ phpHTML_2/writeTimeTXT.php
@@ -353,8 +353,8 @@ if ($_SESSION['isTest']) {
      */
 } else {
     
-    ini_set('display_errors', 1);
-    error_reporting(~0);
+    /*ini_set('display_errors', 1);
+    error_reporting(~0);*/
     include_once 'writeSurveyStringToFile.php';
     /*
      * Notice: default (with 10 main questions) vs. "custom" with just the main
